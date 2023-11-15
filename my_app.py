274f41dc9345e36f788799529bebdd6b01bf2299
Manoj Sangireddy <sangireddymanoj@gmail.com>
2023-11-15 18:28:19 +0530

    Integrated Google OAuth for user authentication

--- my_app.py
+++ my_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 my_app=Flask(__name__)
 @my_app.route('/')
 def index():
@@ -8,3 +9,23 @@ def logout():
     return 'Logged out successfully.'
 if __name__=='__main__':
     my_app.run(debug=True)
+
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app=Flask(__name__)
+app.secret_key="supersecretkey"
+
+blueprint=make_google_blueprint(
+    client_id="",
+    client_secret="",
+    scope=["profile", "email"],
+    redirect_to="google_login",
+    redirect_url="/login/google"
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+@my_app.route("/")
+def index():
+    return redirect(url_for("google.login"))
+if __name__=="__main__":
+    app.run(debug=True)
